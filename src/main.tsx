ff511d487545f815d83928ab48467d0dbc6107b2
housecrystal-18 <housecrystal18@gmail.com>
2025-07-30 14:10:20 -0400

    Switch back to main App with auth fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TestApp } from './TestApp.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TestApp />
+    <App />
   </StrictMode>,
 )
